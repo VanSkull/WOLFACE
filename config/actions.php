a4916355bb14d4f7abdb03e108e8c62fb5497076
Vanhaecke Valentin <63104799+VanSkull@users.noreply.github.com>
2020-11-05 08:45:34 +0100

    Début projet

---
+++ config/actions.php
@@ -0,0 +1,19 @@
+<?php
+// Voici la liste des actions possibles avec la page à charger associée
+
+$listeDesActions = array(
+    "accueil" => "vues/accueil.php",
+    "login" => "vues/login.php",
+    "logup" => "vues/logup.php",
+    "connexion" => "traitement/connexion.php",
+    "deconnexion" => "traitement/deconnexion.php",
+    "create" => "traitement/createAccount.php",
+    "addLike" => "traitement/addLike.php",
+    "delLike" => "traitement/deleteLike.php",
+    "addPost" => "traitement/addPost.php",
+    "modPost" => "traitement/modifyPost.php",
+    "delPost" => "traitement/deletePost.php",  
+    "addComment" => "traitement/addComment.php",
+    "modComment" => "traitement/modifyComment.php",
+    "delComment" => "traitement/deleteComment.php"  
+);
