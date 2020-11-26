8a2b09a0ba377c97836319a12276893054ab6b92
Valentin Vanhaecke <valentinvanh@gmail.com>
2020-11-26 14:35:17 +0100

    Changement BDD

--- wolface_bdd.sql
+++ wolface_bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 26 nov. 2020 à 13:25
+-- Généré le :  jeu. 26 nov. 2020 à 13:31
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.4.0
 
