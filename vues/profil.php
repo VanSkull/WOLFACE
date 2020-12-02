09fa23d0912416f7ab529a02523b3247e772d16f
Valentin Vanhaecke <valentinvanh@gmail.com>
2020-12-02 20:16:02 +0100

    Finalisation posts et commentaires sur page profil

--- vues/profil.php
+++ vues/profil.php
@@ -213,7 +213,7 @@
                     <h3 id="post-titre">Mes posts</h3>
                     <?php
                         //Liste des posts
-                        $sql_posts = "SELECT * FROM posts WHERE idAuteur=? OR idAmi=?";
+                        $sql_posts = "SELECT posts.*, users.*, posts.id AS IDPost FROM posts JOIN users ON users.id=posts.idAmi WHERE posts.idAuteur=? OR posts.idAmi=?";
 
                         $q_posts = $pdo->prepare($sql_posts);
                         
@@ -230,15 +230,49 @@
                                 <img class="photo-auteur" src="images/img_profil.png" alt="Photo_de_profil_de_#" />
                             </div>
                             <div class="text-post">
-                                <p class="nom-auteur">PRENOM NOM</p>
+                                <p class="nom-auteur"><?php echo $line_posts["family_name"]." ".$line_posts["user_name"]; ?></p>
                                 <p class="titre-auteur"><?php echo $line_posts["title"]; ?></p>
                                 <p class="post-auteur"><?php echo $line_posts["content"]; ?></p>
-                                <p class="date-post">Posté par PRENOM NOM le <?php echo $line_posts["datePost"]; ?></p>
+                                <p class="date-post">Posté par <?php echo $line_posts["family_name"]." ".$line_posts["user_name"]; ?> le <?php echo $line_posts["datePost"]; ?></p>
                             </div>
                         </div>
                         <div class="commentaire-post">
                             <h4 class="titre-commentaire">Commentaires</h4>
+                            
+                            <div id="ecrit-commentaire">
+                                <form action="index.php?action=ajoutCommentaire" method="post">
+                                    <textarea id="content" name="content" placeholder="Écrivez votre poste ici..." required></textarea>
+                                    <input type="hidden" name="idPost" value="<?php echo $line_posts["IDPost"]; ?>" />
+                                    <input type="submit" value="Envoyer">
+                                </form>
+                            </div>
+                            
+                            <?php
+                                //Liste des posts
+                                $sql_comments = "SELECT comments.*, users.* FROM comments JOIN users ON users.id=comments.idUser WHERE comments.idPost=?";
+
+                                $q_comments = $pdo->prepare($sql_comments);
+
+                                $q_comments->execute(array($line_posts["IDPost"]));
+
+                                while($line_comments = $q_comments->fetch()){
+                                    /*echo "<pre>";
+                                    var_dump($line_comments);
+                                    echo "</pre>";*/
+                            ?>
                             <div class="commentaire">
+                                <div class="photo-commentateur">
+                                    <img class="photo-profil-commentateur" src="images/img_profil.png" alt="Photo_de_profil_de_#" />
+                                </div>
+                                <div class="main-commentaire">
+                                    <p class="commentaire-commentateur"><?php echo $line_comments["content"]; ?></p>
+                                    <p class="date-commentaire">Posté par <?php echo $line_comments["family_name"]." ".$line_comments["user_name"]; ?> le <?php echo $line_comments["dateComment"]; ?></p>
+                                </div>
+                            </div>
+                            <?php
+                                }
+                            ?>
+                            <!--<div class="commentaire">
                                 <div class="photo-commentateur">
                                     <img class="photo-profil-commentateur" src="images/img_profil.png" alt="Photo_de_profil_de_#" />
                                 </div>
@@ -246,16 +280,7 @@
                                     <p class="commentaire-commentateur">Lorem ipsum aaaaaaaaaaa</p>
                                     <p class="date-commentaire">Posté par PRENOM NOM le DATE à HEURE</p>
                                 </div>
-                            </div>
-                            <div class="commentaire">
-                                <div class="photo-commentateur">
-                                    <img class="photo-profil-commentateur" src="images/img_profil.png" alt="Photo_de_profil_de_#" />
-                                </div>
-                                <div class="main-commentaire">
-                                    <p class="commentaire-commentateur">Lorem ipsum aaaaaaaaaaa</p>
-                                    <p class="date-commentaire">Posté par PRENOM NOM le DATE à HEURE</p>
-                                </div>
-                            </div>
+                            </div>-->
                         </div>                            
                     </div>
                     
@@ -264,7 +289,7 @@
                     ?>
                     
                     
-                    <div class="post-perso">
+                    <!--<div class="post-perso">
                         <div class="main-post">
                             <div class="photo-profil-auteur">
                                 <img class="photo-auteur" src="images/img_profil.png" alt="Photo_de_profil_de_#" />
@@ -296,7 +321,7 @@
                                 </div>
                             </div>
                         </div>                            
-                    </div>
+                    </div>-->
                 </div>
             </div>
             <?php
